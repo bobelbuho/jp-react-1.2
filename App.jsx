f0f967a4efa97151d6f8446ead17d41ed7e32a46
CodeSandbox <support@codesandbox.io>
2023-10-23 13:37:15 +0000

    Sandbox snapshot commit

--- App.jsx
+++ App.jsx
@@ -7,22 +7,17 @@ export default function App() {
     <Navbar shouldHideOnScroll>
       <NavbarBrand>
         <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit">Jeppey</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Features
+           Rechercher
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+            Publier un trajet
           </Link>
         </NavbarItem>
       </NavbarContent>
